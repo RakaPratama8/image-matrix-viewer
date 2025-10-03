a863d21cdcaff328a203ff19d9fb2ad8500dd1ba
Muhamad Raka Pratama <raka824pratama@gmail.com>
2025-10-03 22:25:24 +0700

    adding project main component

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import io
+
+
 def main():
-    print("Hello from image-matrix-viewer!")
-
-
+    img_uploaded = st.file_uploader("Upload image here!", type=["jpg", "jpeg", "png"],
+                                    accept_multiple_files=False)
+    
+    column1, column2 = st.columns(2)
+    if img_uploaded:
+        img_bytes = img_uploaded.read()
+        img = Image.open(io.BytesIO(img_bytes))
+        img = img.convert('RGB')
+        img_np = np.asarray(img)
+        
+        table_datas = []
+        for i in range(img_np.shape[0]):
+            datas = []
+            for j in range(img_np[0].shape[0]):
+                datas.append(','.join(map(str, img_np[i][j])))
+            table_datas.append(datas)
+        
+        st.dataframe(table_datas)
+        
+        with column1:
+            st.image(img_np, caption=f"{img_uploaded.name}", use_container_width="always")
+        with column2:
+            x, y = st.number_input("Input x value", step=1, min_value=0, max_value=img_np.shape[0]-1), st.number_input("Input y value", step=1, min_value=0, max_value=img_np.shape[0]-1)
+            
+            if x >= 0 and y >= 0:
+                st.text(
+                    f"Selected element :\n{img_np[x][y]}"
+                )
+            
 if __name__ == "__main__":
     main()
